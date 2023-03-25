38d4d538fcbfa1a501dcd787fbf7addf1cccc7ed
gitDemeke <demeke.asres@aau.edu.et>
2023-03-25 11:07:26 +0300

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
+#!/usr/bin/pup
 # Install the puppet-lint package
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
